38bb1d38a06cee3ac62ba8ca367ca2a62eaf06b3
Cumutoni12 <cumutoni@gmail.com>
2025-02-05 21:29:22 -0500

    updated

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const form = document.querySelector("#form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // retrieve input value
+
   const email = document.querySelector("#email").value;
   const name = document.querySelector("#name").value;
-  const formData={
-    name:name , email:email
-  }
-  localStorage.setItem("formData",JSON.stringify(formData))
-  alert("Data saved to the local storage")
-  form.reset()
-  
+  const formData = {
+    name: name,
+    email: email,
+  };
+  // storing data to the local storage
+  localStorage.setItem("formData", JSON.stringify(formData));
+  alert("Data saved to the local storage");
+  //   form.reset();
 });
 
+// getting data from the local storage
+if (localStorage.getItem("formData")) {
+  const storedData = JSON.parse(localStorage.getItem("formData"));
+  document.querySelector("#name").value = storedData.name;
+  document.querySelector("#email").value = storedData.email;
+}
